db152b72279dcbb8c9d36d026756a76448bd59b8
Steffen Beisenherz <git@beisenherz.dev>
2025-08-13 23:17:41 +0200

    install service monitors and gateway api
    Signed-off-by: Steffen Beisenherz <git@beisenherz.dev>

---
+++ bootstrap/install-gateway-crds.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+function apply_crds() {
+	local -r crds=(
+		# renovate: datasource=github-releases depName=prometheus-operator/prometheus-operator
+		https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.84.0/stripped-down-crds.yaml
+		# renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
+		https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/experimental-install.yaml
+	    )
+
+	    for crd in "${crds[@]}"; do
+	if kubectl diff --filename "${crd}" &>/dev/null; then
+		    echo "INFO: CRDs are up-to-date crd=${crd}"
+		    continue
+		fi
+		if kubectl apply --server-side --filename "${crd}" &>/dev/null; then
+		    echo "INFO: CRDs applied crd=${crd}"
+		else
+		    echo "ERROR: Failed to apply CRDs crd=${crd}"
+		fi
+	done
+}
+
+function main() {
+	apply_crds
+}
+
+main "$@"
